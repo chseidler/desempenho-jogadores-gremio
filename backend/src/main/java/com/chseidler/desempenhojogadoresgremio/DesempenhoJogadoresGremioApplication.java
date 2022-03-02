854879d6612ff3e88061ba041753d2b14fc2d262
Christian Seidler <ch.cseidler@gmail.com>
2022-03-02 11:58:42 -0300

    Inicialização do Spring e React

---
+++ backend/src/main/java/com/chseidler/desempenhojogadoresgremio/DesempenhoJogadoresGremioApplication.java
@@ -0,0 +1,13 @@
+package com.chseidler.desempenhojogadoresgremio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesempenhoJogadoresGremioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesempenhoJogadoresGremioApplication.class, args);
+	}
+
+}
