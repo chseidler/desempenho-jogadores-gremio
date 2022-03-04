e21dd273b74e94757ab1183579317a1003c279e2
Christian Seidler <ch.cseidler@gmail.com>
2022-03-04 15:49:46 -0300

    ajustado ordem no banco de dados

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -4,41 +4,43 @@ INSERT INTO tb_user(email) VALUES ('vagner@gmail.com');
 INSERT INTO tb_user(email) VALUES ('eduardo@gmail.com');
 INSERT INTO tb_user(email) VALUES ('christian@gmail.com');
 
-INSERT INTO tb_player(score, count, name, position, image) VALUES (4.5, 2, 'Brenno', 'Goleiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-760af28962cdf938537a5e131492d91b.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (0.1, 100, 'Brenno', 'Goleiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-760af28962cdf938537a5e131492d91b.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (1, 100, 'Orejuela', 'Lateral', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-c6b0ef8455db831579c3b1440640884f.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (2, 100, 'Geromel', 'Zagueiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-b76a63cbf275110ad3c4df8f72c5baef.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (3, 100, 'Bruno Alves', 'Zagueiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-f3d9c4c4b192f3e137ff1d715325e580.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (3, 100, 'Nicolas', 'Lateral', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-83aac4355acf65d42ff57a2aa69477bc.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (.1, 100, 'Thiago Santos', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-a44c714051a702a203b55e8b01df0ed7.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (2, 100, 'Bitello', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-46e8c7754d18d7cff68ed2646ab4ab44.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (2, 100, 'Gabriel Silva', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-a612d55ed9e252299ebd02ba9e60ff80.JPG');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (2, 100, 'Janderson', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-8ee82c303a8269d0b1df79b820c597af.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (2, 100, 'Rildo', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-a512623a42c00eadf84e2c1423cf9c65.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (3, 100, 'Diego Souza', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-d7c3e05b29cbc729c4ab0bab2e36f7c3.JPG');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (1, 100, 'Roger Machado', 'Treinador', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-23d2f15e9e94335579a985a8357e705d.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (1, 100, 'Lucas Silva', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-212a9f9a68dfe1b26328380efde5e392.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (1, 100, 'Benitez', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-213eefc0b0ec47fcea7cf9051042957b.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (2, 100, 'Campaz', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-ec814358648e7315ec12c0ae89644a53.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (2, 100, 'Elias Manoel', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-19e21b9d9c362a06bf6fa54efbea204a.jpg');
+INSERT INTO tb_player(score, count, name, position, image) VALUES (0.1, 100, 'Churín', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-de91bc4de75d1566580801072e0039f7.jpg');
+
 INSERT INTO tb_player(score, count, name, position, image) VALUES (3.3, 3, 'Gabriel Grando', 'Goleiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-1397cfa506b36be639eb9cd5f36d2251.jpg');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Felipe Scheibig', 'Goleiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-52412bfe5ad677a5079fa97e1a6351fa.JPG');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Adriel', 'Goleiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-baa26ef0f27845318aabac630ba0e0e5.jpg');
 
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Leo Gomes', 'Lateral', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-6c975f56d83671bd2e3fcc16dafbcb4b.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Orejuela', 'Lateral', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-c6b0ef8455db831579c3b1440640884f.jpg');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Diogo Barbosa', 'Lateral', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-db13a61e7cf9517202610ca9c64d5423.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Nicolas', 'Lateral', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-83aac4355acf65d42ff57a2aa69477bc.jpg');
 
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Geromel', 'Zagueiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-b76a63cbf275110ad3c4df8f72c5baef.jpg');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Kannemann', 'Zagueiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-eb94e01792434e2a8a92b221e8412bce.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Bruno Alves', 'Zagueiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-f3d9c4c4b192f3e137ff1d715325e580.jpg');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Rodrigues', 'Zagueiro', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-923304a54c4d7132289d8cecd1a0c3c6.jpg');
 
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Villasanti', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-44d58734baf4d961f82953f3ddc05c6a.jpg');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Fernando Henrique', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-ed88b623064f4705c565ecab0e46a59e.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Lucas Silva', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-212a9f9a68dfe1b26328380efde5e392.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Thiago Santos', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-a44c714051a702a203b55e8b01df0ed7.jpg');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Sarará', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-d71555c3a3f3e830bf51ac3fd3392a7a.jpg');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Victor Bobsin', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-fe6564cb6f50f056783142e9b7a87779.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Benitez', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-213eefc0b0ec47fcea7cf9051042957b.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Campaz', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-ec814358648e7315ec12c0ae89644a53.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Gabriel Silva', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-a612d55ed9e252299ebd02ba9e60ff80.JPG');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Pedro Lucas', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-e9ed2273ad9bcae4a90cdff0dad945b7.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Rildo', 'Meio Campo', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-a512623a42c00eadf84e2c1423cf9c65.jpg');
 
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Diego Souza', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-d7c3e05b29cbc729c4ab0bab2e36f7c3.JPG');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Churín', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-de91bc4de75d1566580801072e0039f7.jpg');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Jhonata Robert', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-d694b5b1311fd31bbc16db2a153b4c8f.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Elias Manoel', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-19e21b9d9c362a06bf6fa54efbea204a.jpg');
 INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Ferreira', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-4bda6e8dcda2bdd3c91e102570682ab1.jpg');
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Janderson', 'Atacante', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-8ee82c303a8269d0b1df79b820c597af.jpg');
 
-INSERT INTO tb_player(score, count, name, position, image) VALUES (0, 0, 'Roger Machado', 'Treinador', 'https://gremio.blob.core.windows.net/imgs-site/md-jogador-acao-23d2f15e9e94335579a985a8357e705d.jpg');
 
 INSERT INTO tb_score(player_id, user_id, value) VALUES (1, 1, 5.0);
 INSERT INTO tb_score(player_id, user_id, value) VALUES (1, 2, 4.0);
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -14,7 +14,7 @@ function Listing() {
         last: true,
         totalPages: 0,
         totalElements: 0,
-        size: 12,
+        size: 17,
         number: 0,
         first: true,
         numberOfElements: 0,
@@ -22,7 +22,7 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/players?size=12&page=${pageNumber}`)
+        axios.get(`${BASE_URL}/players?size=17&page=${pageNumber}`)
             .then(response => {
                 const data = response.data as PlayerPage;
                 setPage(data);
